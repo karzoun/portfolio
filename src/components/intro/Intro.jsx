29e6a000e7c0983d1b3cb1313419a1d543e59ff9
“karzoun” <karzoun_27@hotmail.com>
2022-03-06 21:41:19 -0800

    portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,19 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 50,
+
+      strings: ["JavaScript", "ReactJs!", "HTML"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -13,7 +26,7 @@ export default function Intro() {
           <h2>Hi There, I'm</h2>
           <h1>Ahmad Al-Karzoun</h1>
           <h3>
-            Full Stack Developer <span></span>
+            Full Stack Developer <span ref={textRef}></span>
           </h3>
         </div>
         <a href="#portfolio">
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,25 @@
 import "./portfolio.scss";
 
 export default function Portfolio() {
-  return <div className="portfolio" id="portfolio"></div>;
+  return (
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li>Featured</li>
+        <li>Web App</li>
+        <li>Desing</li>
+        <li>Brabding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            className="wetherImg"
+            src="https://media.wired.co.uk/photos/606dba1c9a15f73a597a2aa1/4:3/w_2044,h_1533,c_limit/weather.jpg"
+            alt=""
+          />
+          <h3>Weather App</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
