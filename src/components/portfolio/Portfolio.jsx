867143be5770d8818cf971ff9d2be21eb036e6f5
“karzoun” <karzoun_27@hotmail.com>
2022-03-08 01:24:44 -0800

    portfolio2

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,8 +4,8 @@ export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
-      <ul>
-        <li>Featured</li>
+      <ul className="ulP">
+        <li className="active">Featured</li>
         <li>Web App</li>
         <li>Desing</li>
         <li>Brabding</li>
@@ -17,7 +17,16 @@ export default function Portfolio() {
             src="https://media.wired.co.uk/photos/606dba1c9a15f73a597a2aa1/4:3/w_2044,h_1533,c_limit/weather.jpg"
             alt=""
           />
-          <h3>Weather App</h3>
+          <h3 className="hP">Weather App</h3>
+        </div>
+
+        <div className="item">
+          <img
+            className="wetherImg"
+            src="https://mfallesen.github.io/plannerly/assets/images/pexels-andrea-piacquadio-842546.jpg"
+            alt=""
+          />
+          <h3 className="hP">Weather App</h3>
         </div>
       </div>
     </div>
