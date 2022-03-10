3a5a07d9b40224a310db9b5a11fe1ef5577a0ba0
“karzoun” <karzoun_27@hotmail.com>
2022-03-10 01:30:20 -0800

    portfolioList

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,32 @@
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "web App",
+    },
+    {
+      id: "desing",
+      title: "Desing",
+    },
+    {
+      id: "barbding",
+      title: "Barbding",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Portfolio</h1>
       <ul className="ulP">
-        <li className="active">Featured</li>
-        <li>Web App</li>
-        <li>Desing</li>
-        <li>Brabding</li>
+        {list.map((item) => (
+          <PortfolioList title={item.title} />
+        ))}
       </ul>
       <div className="container">
         <div className="item">
@@ -26,7 +44,22 @@ export default function Portfolio() {
             src="https://mfallesen.github.io/plannerly/assets/images/pexels-andrea-piacquadio-842546.jpg"
             alt=""
           />
-          <h3 className="hP">Weather App</h3>
+
+          <h3 className="hP">Make my Date</h3>
+        </div>
+        <div className="item">
+          <img
+            className="wetherImg"
+            src="https://pocketbutler.herokuapp.com/assets/logo.png"
+            alt=""
+          />
+
+          <h3 className="hP">Pocket Butler</h3>
+        </div>
+        <div className="item">
+          <img className="wetherImg" src="" alt="" />
+
+          <h3 className="hP">Make my Date</h3>
         </div>
       </div>
     </div>
---
+++ src/components/portfolioList/PortfolioList.jsx
@@ -0,0 +1,5 @@
+import "./portfolioList.scss";
+
+export default function PortfolioList({ title }) {
+  return <li className="portfolioList">{title}</li>;
+}
